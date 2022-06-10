38cee5d80ef2b782844e93ce2067f1d1c0db2ff1
Lucas <lucasnun3s@ucl.br>
2022-06-10 20:10:54 -0300

    ate aqui tudo bem

---
+++ p3pa/biblioteca.h
@@ -0,0 +1,12 @@
+#ifndef __BIBLIOTECA_H
+#define __BIBLIOTECA_H
+
+typedef struct mxmn{
+    int max;
+    int min;
+    int qtd;
+}tMaximos;
+
+void vetor(int* p, int tam, &mx, &mn);
+
+#endif
---
+++ p3pa/mainp3.c
@@ -0,0 +1,12 @@
+//    gcc -c arqfonte.c
+//    gcc -c main.c
+//    gcc -o main.exe main.o arqfonte.o
+
+//    gcc -c -g* .c
+//   gcc*.o -o programa
+//    .\programa.exe
+
+#include ".h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
